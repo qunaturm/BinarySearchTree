51f5c2ba753920d92a3fa482a430209c66c2a1cf
Хисматулина Карина <hkarinaf@gmail.com>
2020-05-05 21:59:09 +0300

    Add project files.

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class Node<T> : IComparable<T>, IComparable where T : IComparable 
+    {
+        public T Data { get; private set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        public Node(T data, Node<T> left, Node<T> right)
+        {
+            Data = data;
+            Left = left;
+            Right = right;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is Node<T>)
+            {
+                return Data.CompareTo(obj);
+            }
+
+            else throw new Exception("Wrong data type");
+            
+        }
+
+        public int CompareTo([AllowNull] T other)
+        {
+            return Data.CompareTo(other);
+        }
+    }
+}
---
+++ BinarySearchTree/Tree.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class Tree<T> where T : IComparable
+    {
+        public Node<T> Root { get; private set; }
+        public int Count { get; private set; }
+
+        public void Add(T data)
+        {
+            var node = new Node<T>(data);
+
+            if (Root == null)
+            {
+                Root = node;
+                Count = 1;
+                return;
+            }
+
+            if (Root != null)
+            {
+                var current = Root;
+
+                if (node.CompareTo(current) < 0) //this > new
+                {
+                    if (current.Left == null)
+                    {
+                        current.Left = node;
+                        Count += 1;
+                        return;
+                    }
+                    else
+                    {
+                        while(current.Left != null)
+                        {
+                            current = current.Left;
+                        }
+                        current.Left = node;
+                        return;
+                    }
+                }
+
+                if (node.CompareTo(current) == 0) //this == new
+                {
+
+                }
+
+                if (node.CompareTo(current) > 0) //this < new
+                {
+                    if (current.Right == null)
+                    {
+                        current.Right = node;
+                        Count += 1;
+                        return;
+                    }
+                    else
+                    {
+                        while(current.Right != null)
+                        {
+                            current = current.Right;
+                        }
+                        current.Right = node; 
+                    }
+                }
+            }
+
+        }
+    }
+}
